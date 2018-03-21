84ff85e762eaa67d58dd51d4f0226e291cebc56f
IntoThePit <theblindorphan@gmail.com>
2018-03-21 23:17:47 +0000

    Added synonyms to system. Updated initial report

--- utils/baseline.py
+++ utils/baseline.py
@@ -4,6 +4,9 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from nltk import word_tokenize, pos_tag, download
 from nltk.data import load
+from nltk.corpus import wordnet
+
+
 
 class Baseline(object):
 
@@ -17,10 +20,6 @@ class Baseline(object):
 
 #        self.model = RandomForestClassifier(n_estimators = 20)
         self.model = LogisticRegression()
-
-    def extract_features(self, word):
-        len_chars = len(word) / self.avg_word_length
-        len_tokens = len(word.split(' '))
         
         # This one didn't seem to do anything.
         l_rarity = defaultdict(float, 
@@ -53,6 +52,86 @@ class Baseline(object):
                                  ('e' , 12.702),
                                 )
                                )
+        for k in l_rarity:
+            l_rarity[k] = 1/(l_rarity[k]**2)
+            
+        self.letter_rarity = l_rarity
+        
+        sp_consonants = set(("v", "ll", "h", "j", "r", "rr", "z", "ñ", "x"))
+        
+#        Vowel diagraphs: ou, ow, eigh, au, aw, oo
+#        Consonant digraphs: sh, th, wh, ph
+#        Consonant blends: sl, sm, sts, scr, spr, str
+#        Initial sounds: kn, qu, wr, sk
+#        Final sounds: ck, ng, gh
+#        Endings: -ed (pronounced /d/ or /t/ or /ded/ or /ted/)
+#        Endings: -s (pronounced /s/ or /z/ or /ez/ or /es/)
+#        Endings without a vowel: -ps, -ts
+#        Suffixes/prefixes: un-, over-, under-, -ly, -ness, -ful, -est
+#        http://www.colorincolorado.org/article/capitalizing-similarities-and-differences-between-spanish-and-english
+        
+        eng_v_diagraphs = set(("ou", "ow", "eigh", "au", "aw", "oo"))
+        eng_c_diagraphs = set(("sh", "th", "wh", "ph"))
+        eng_c_blends = set(("sl", "sm", "sts", "scr", "spr", "str"))
+        
+        self.inside_check = list(sp_consonants | eng_v_diagraphs | eng_c_diagraphs | eng_c_blends)
+        
+        eng_init_sounds = set(("kn", "qu", "wr", "sk"))
+        eng_prefixes = set(("un","over","under"))
+        
+        sp_prefixes = set()    
+        with open("spprefixes.txt") as file:
+            for line in file:
+                for w in line.split():
+                    sp_prefixes.add(w)
+                    
+        eng_prefixes_lat = set()
+        with open("latinGreekPrefixes.txt") as file:
+            for line in file:
+                for w in line.split():
+                    eng_prefixes_lat.add(w)
+        
+        self.prefix_check = list(eng_prefixes | eng_init_sounds | eng_prefixes | eng_prefixes_lat)
+        
+        eng_final_sounds = set(("ck", "ng", "gh"))
+        eng_endings = set(("ed","s","ps","ts"))
+        
+        eng_suffixes = set()    
+        with open("suffixes.txt") as file:
+            for line in file:
+                for w in line.split():
+                    eng_suffixes.add(w)
+                    
+        sp_suffixes = set()    
+        with open("spsuffixes.txt") as file:
+            for line in file:
+                for w in line.split():
+                    sp_suffixes.add(w)
+                    
+#        Adding spanish suffixes made it worse!
+        self.suffix_check = list(eng_suffixes | eng_final_sounds | eng_endings | sp_suffixes)
+           
+        self.prefix_vect = np.array(np.zeros(len(self.prefix_check)))          
+        self.inside_vect = np.array(np.zeros(len(self.inside_check)))
+        self.suffix_vect = np.array(np.zeros(len(self.suffix_check)))
+        
+        tag_keys = load('help/tagsets/upenn_tagset.pickle').keys()
+        
+        self.tag_vect_template = {}
+        tag_id = 0
+        for tag_key in tag_keys:
+            self.tag_vect_template[tag_key] = tag_id
+            tag_id += 1
+            
+        self.tag_vect = np.array(np.zeros(len(tag_keys)))
+
+    def extract_features(self, word):
+        len_chars = len(word) / self.avg_word_length
+        len_tokens = len(word.split(' '))
+        
+        # Add all of the pre-processing to the init area
+        
+        
         r_score = 0.0
         vowels = ['a','e','i','o','u']
 
@@ -64,7 +143,7 @@ class Baseline(object):
         
         for l in word.lower():
             
-            r_score += l_rarity[l]
+            r_score += self.letter_rarity[l]
             
             if l in vowels:
                 
@@ -96,108 +175,75 @@ class Baseline(object):
                 
         r_score_norm = r_score/len(word)
         
-        sp_consonants = set(("v", "ll", "h", "j", "r", "rr", "z", "ñ", "x"))
-        
-#        Vowel diagraphs: ou, ow, eigh, au, aw, oo
-#        Consonant digraphs: sh, th, wh, ph
-#        Consonant blends: sl, sm, sts, scr, spr, str
-#        Initial sounds: kn, qu, wr, sk
-#        Final sounds: ck, ng, gh
-#        Endings: -ed (pronounced /d/ or /t/ or /ded/ or /ted/)
-#        Endings: -s (pronounced /s/ or /z/ or /ez/ or /es/)
-#        Endings without a vowel: -ps, -ts
-#        Suffixes/prefixes: un-, over-, under-, -ly, -ness, -ful, -est
-#        http://www.colorincolorado.org/article/capitalizing-similarities-and-differences-between-spanish-and-english
-        
-        eng_v_diagraphs = set(("ou", "ow", "eigh", "au", "aw", "oo"))
-        eng_c_diagraphs = set(("sh", "th", "wh", "ph"))
-        eng_c_blends = set(("sl", "sm", "sts", "scr", "spr", "str"))
-        
-        inside_check = list(sp_consonants | eng_v_diagraphs | eng_c_diagraphs | eng_c_blends)
-        
-        eng_init_sounds = set(("kn", "qu", "wr", "sk"))
-        eng_prefixes = set(("un","over","under"))
-        
-        sp_prefixes = set()    
-        with open("spprefixes.txt") as file:
-            for line in file:
-                for w in line.split():
-                    sp_prefixes.add(w)
-        
-        prefix_check = list(eng_prefixes | eng_init_sounds | eng_prefixes)
-        
-        eng_final_sounds = set(("ck", "ng", "gh"))
-        eng_endings = set(("ed","s","ps","ts"))
-        
-        eng_suffixes = set()    
-        with open("suffixes.txt") as file:
-            for line in file:
-                for w in line.split():
-                    eng_suffixes.add(w)
-                    
-        sp_suffixes = set()    
-        with open("spsuffixes.txt") as file:
-            for line in file:
-                for w in line.split():
-                    sp_suffixes.add(w)
-                    
-#        Adding spanish suffixes made it worse!
-        suffix_check = list(eng_suffixes | eng_final_sounds | eng_endings | sp_suffixes)
-           
-        prefix_vect = np.array(np.zeros(len(prefix_check)))          
-        inside_vect = np.array(np.zeros(len(inside_check)))
-        suffix_vect = np.array(np.zeros(len(suffix_check)))
-        
-        
 #        test_words = word.split(' ')
         test_words = word_tokenize(word)
         test_tags = [x[1] for x in pos_tag(test_words)]
-        tag_keys = load('help/tagsets/upenn_tagset.pickle').keys()
         
-        tag_vect_template = {}
-        tag_id = 0
-        for tag_key in tag_keys:
-            tag_vect_template[tag_key] = tag_id
-            tag_id += 1
-            
-        tag_vect = np.array(np.zeros(len(tag_keys)))
-            
+        self.tag_vect.fill(0)
+        
         for tag in test_tags:
-            if tag not in tag_vect_template:
+            if tag not in self.tag_vect_template:
                 print("Error! Didn't find tag! This shouldn't happen.")
             else:
-                tag_vect[tag_vect_template[tag]] = 1
+                self.tag_vect[self.tag_vect_template[tag]] = 1
                 
-        for word in test_words:
+        self.prefix_vect.fill(0)
+        self.inside_vect.fill(0)  
+        self.suffix_vect.fill(0)
+        
+        for cased_word in test_words:
+            
+            word = cased_word.lower()
+            
+            num_synonyms = len(wordnet.synsets(word))
             
             prefix_index = 0
-            for prefix in prefix_check:
+            for prefix in self.prefix_check:
                 if word.startswith(prefix):
-                    prefix_vect[prefix_index] = 1
+                    self.prefix_vect[prefix_index] = 1
                 prefix_index += 1
                 
             inside_index = 0
-            for inside in inside_check:
+            for inside in self.inside_check:
                 if inside in word:
-                    inside_vect[inside_index] = 1
+                    self.inside_vect[inside_index] = 1
                 inside_index += 1
             
             # So far, none of these features improve prediction.
             
             suffix_index = 0
-            for suffix in suffix_check:
+            for suffix in self.suffix_check:
                 if word.endswith(suffix):
-                    suffix_vect[suffix_index] = 1
+                    self.suffix_vect[suffix_index] = 1
                 suffix_index += 1
         
         result = []
         result.append(len_chars)
         result.append(len_tokens)
         result.append(r_score_norm)
-        result.append(max_cons_v)
-        result.append(max_cons_c)
+#        result.append(max_cons_v)
+#        result.append(max_cons_c)
+#        result.append(num_synonyms)
+#        
+#        result = np.hstack((result, self.suffix_vect, self.inside_vect, self.prefix_vect, self.tag_vect))
         
-        result = np.hstack((result, suffix_vect, inside_vect, prefix_vect, tag_vect)) 
+#        if word == "biology" or word == "neurons" or word == "disruption":
+#            print(word)
+#            
+#            print("Suffixes:")
+#            for i in range(len(self.suffix_check)):
+#                if self.suffix_vect[i] == 1:
+#                    print(self.suffix_check[i], self.suffix_vect[i])
+#                
+#            print("\nPrefixes:")    
+#            for i in range(len(self.prefix_check)):
+#                if self.prefix_vect[i] == 1:
+#                    print(self.prefix_check[i], self.prefix_vect[i])
+#            
+#            print("\nInfixes:")    
+#            for i in range(len(self.inside_check)):
+#                if self.inside_vect[i] == 1:
+#                    print(self.inside_check[i], self.inside_vect[i])
 
         return result
 
